9aa0dc20f0c4846bb5a8cf5762851fd208fa6788
Rerange <78526012+Quandong-Zhang@users.noreply.github.com>
2022-07-31 22:01:48 +0800

    Update new_downloader.py

--- new_downloader.py
+++ new_downloader.py
@@ -1,4 +1,5 @@
 import yt_dlp
+import requests
 import json
 import time
 import os
@@ -27,6 +28,21 @@ def get_info(url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
         return info
+    
+def getVideoPath(id_):  
+    path="./"+str(id_)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.find(id_)!=-1:
+                return os.path.join(root,file)
+
+def download_image(url,id_):
+    r = requests.get(url, stream=True)
+    f = open("./"+str(id_)+"/cover.webp", "wb")
+    # chunk是指定每次写入的大小，每次只写了100kb
+    for chunk in r.iter_content(chunk_size=102400):
+        if chunk:
+            f.write(chunk)
 
 def main(vUrl):
     info = get_info(vUrl)
@@ -35,10 +51,15 @@ def main(vUrl):
     id_ = info['id']
     description = info['description']
     tags = info['tags']
+    cover = info['thumbnail']
     tags.append(author)
     tags.append(OWNER_NAME)
-    os.mkdir(path='./'+str(id_))
+    try:
+        os.mkdir(path='./'+str(id_))
+    except FileExistsError:
+        shutil.rmtree('./'+str(id_))
     download(vUrl,id_)
+    download_image(cover, id_)
     title=re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a\u3040-\u31FF\uFF00-\uFFA0\u0020\u3000])", '', title)
     if len(title)>=80:
         title=title[:80]
@@ -46,11 +67,22 @@ def main(vUrl):
         description=description[:250]
     if len(tags)>=10:
         tags=tags[:10]
-    videoRelativePath=os.walk(top='./'+str(id_))[2][0]
-    videoPath=os.path.join("./",videoRelativePath)
-    CMD="./biliup upload "+videoPath+" --desc "+get_double(description)+" --copyright 2 --tag "+get_double(str(tags)[1:-2]+",youtube")+" --tid "+str(TID)+" --source "+get_double(vUrl)+" --line cos --dynamic "+get_double("@"+str(OWNER_NAME))+" --title "+get_double(title)
+    i=0
+    while len(tags)>i:
+        if len(tags[i])>20:
+            tags[i]=tags[i][:20]
+        i+=1
+    strTags = ','.join(tags)
+    videoPath=getVideoPath(id_)
+    CMD="./biliup upload "+videoPath+" --desc "+get_double(description)+" --copyright 2 --tag "+get_double(strTags+",youtube")+" --tid "+str(TID)+" --source "+get_double(vUrl)+" --line cos --dynamic "+get_double("@"+str(OWNER_NAME))+" --title "+get_double(title)+" --cover "+str("./"+str(id_)+"/cover.webp")
     print(CMD)
-    os.system(CMD)
+    biliupOutput="".join(os.popen(CMD).readlines())
+    if biliupOutput.find("投稿成功")==-1:
+        print(biliupOutput)
+        print("投稿失败,是bug或biliup出了问题。ask issues on https://github.com/Quandong-Zhang/youtube-trans-bot/issues or https://github.com/ForgQi/biliup-rs/issues ")
+        shutil.rmtree('./'+str(id_))
+        exit(1)
+    print("投稿成功")
     shutil.rmtree('./'+str(id_))
 
 if __name__ == '__main__':
